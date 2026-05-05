f82dcdb41d03f0dcc56dc099523c49e841287de9
Emmanuel León <cn-emmanuel@hotmail.com>
2026-05-05 16:38:12 -0600

    Added MongoDB connection and user schema

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
 require('dotenv').config();
 
 app.use(express.json()); // For requests with JSON
+
+// Just apply DNS on local development
+if (process.env.NODE_ENV !== "production") {
+  try {
+    require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
+  } catch(error) {
+    console.log("DNS fix not needed in production");
+  }
+}
+
+connectDB();
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'API running' });
---
+++ src/controllers/authController.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
