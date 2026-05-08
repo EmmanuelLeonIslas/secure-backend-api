2381a185249438b59b8e07207d1174ffb67b3bc6
Emmanuel León <cn-emmanuel@hotmail.com>
2026-05-08 15:05:32 -0600

    Added authMiddleware.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -44,20 +44,3 @@ exports.login = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-exports.profile = async (req, res) => {
-  try {
-    const authHeader = req.headers.authorization;
-    const token = authHeader?.split(' ')[1];
-
-    if (!token || !token)
-      return res.status(401).json({ message: 'Token not provided' });
-
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    const id = payload.id;
-
-    res.status(200).json({ message: 'Access granted!' });
-  } catch (error) {
-    res.status(401).json({ error: error.message });
-  }
-};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { login, profile, register } = require('../controllers/authController');
+const { login, register } = require('../controllers/authController');
+const { profile } = require('../middlewares/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
